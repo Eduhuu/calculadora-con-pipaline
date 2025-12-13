c0ba031d3f5b18d544e7651c69e9533fa9398335
Eduhuu <eduardosuarez.c97@gmail.com>
2025-12-13 11:33:29 +0000

    add test code for division with one negative and one positive interge

--- src/actions/__tests__/dividir.test.ts
+++ src/actions/__tests__/dividir.test.ts
@@ -4,5 +4,9 @@ describe('dividir', () => {
   it('debe dividir dos números positivos correctamente', async () => {
     expect(await dividir(6, 3)).toBe(2);
   });
+  
+  it('debe dividir un número positivo entre uno negativo', async () => {
+    expect(await dividir(6, -3)).toBe(-2);
+  });
 });
 
