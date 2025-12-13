af63644deb57229e423352a5af81acdbf7e1285f
Eduhuu <eduardosuarez.c97@gmail.com>
2025-12-13 11:35:40 +0000

    add code for manage all tests

--- src/actions/dividir.ts
+++ src/actions/dividir.ts
@@ -8,6 +8,12 @@
  * @throws Error si el divisor es cero
  */
 export async function dividir(a: number, b: number): Promise<number> {
+  if (b === 0) {
+    throw new Error('No se puede dividir por cero');
+  }
+  if (a === 0) {
+    return 0;
+  }
     return a / b;
 }
 
